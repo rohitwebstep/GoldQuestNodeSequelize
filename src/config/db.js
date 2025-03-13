e44318b5c16255e07513b6d60ef3ec0472989a89
rohitwebstep <rohitwebstep@gmail.com>
2025-03-13 10:46:56 +0530

    update

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("goldquest_node", "root", "", {
-  host: "localhost",   // Change this if using a remote DB
+const sequelize = new Sequelize("goldquest_sequelize", "goldquest_db", "GoldQuest@135", {
+  host: "127.0.0.1",   // Change this if using a remote DB
   dialect: "mysql",    // Set the dialect (mysql, postgres, sqlite, mssql)
   logging: false,      // Optional: Disable console logs
 });
