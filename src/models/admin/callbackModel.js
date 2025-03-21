b9d2a220bc36a18e5f9ffa6ae99ac036aa426ed5
rohitwebstep <rohitwebstep@gmail.com>
2025-03-21 14:29:09 +0530

    implemented on callcack

--- src/models/admin/callbackModel.js
+++ src/models/admin/callbackModel.js
@@ -1,38 +1,33 @@
-const { pool, startConnection, connectionRelease } = require("../../config/db");
+const { sequelize } = require("../../config/db");
+const { QueryTypes } = require("sequelize");
 
 const Callback = {
-  list: (callback) => {
-    const sql = `
-      SELECT 
-        cr.*,
-        b.name AS branch_name, 
-        c.name AS customer_name, 
-        c.mobile AS mobile, 
-        cm.single_point_of_contact AS single_point_of_contact 
-      FROM \`callback_requests\` cr
-      INNER JOIN \`branches\` b ON b.id = cr.branch_id
-      INNER JOIN \`customers\` c ON c.id = cr.customer_id
-      INNER JOIN \`customer_metas\` cm ON cm.customer_id = cr.customer_id
-      ORDER BY cr.\`requested_at\` DESC
-    `;
+  list: async (callback) => {
+    try {
+      const sql = `
+            SELECT 
+                cr.*,
+                b.name AS branch_name, 
+                c.name AS customer_name, 
+                c.mobile AS mobile, 
+                cm.single_point_of_contact AS single_point_of_contact 
+            FROM \`callback_requests\` cr
+            INNER JOIN \`branches\` b ON b.id = cr.branch_id
+            INNER JOIN \`customers\` c ON c.id = cr.customer_id
+            LEFT JOIN \`customer_metas\` cm ON cm.customer_id = cr.customer_id
+            ORDER BY cr.\`requested_at\` DESC
+        `;
 
-    startConnection((err, connection) => {
-      if (err) {
-        return callback(err, null);
-      }
-
-      connection.query(sql, (queryErr, results) => {
-        connectionRelease(connection); // Release the connection
-
-        if (queryErr) {
-          console.error("Database query error: 47", queryErr);
-          return callback(queryErr, null);
-        }
-
-        callback(null, results);
+      const results = await sequelize.query(sql, {
+        type: QueryTypes.SELECT,
       });
-    });
+
+      callback(null, results);
+    } catch (error) {
+      callback(error, null);
+    }
   },
+
 };
 
 module.exports = Callback;
